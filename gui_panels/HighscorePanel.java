c253c89a6db393decf091ce89ac51493f6d306ba
Andrew-Lee-A <79880562+Andrew-Lee-A@users.noreply.github.com>
2021-05-18 16:02:30 +1200

    Update highscorePanel
    Changed styling, and retrieves data from database

--- gui_panels/HighscorePanel.java
+++ gui_panels/HighscorePanel.java
@@ -7,19 +7,16 @@ package gui_panels;
 
 import animation.GameState;
 import controllers.HighscoreController;
-import controllers.LoginController;
+import game_db.GameDBManager;
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.Graphics;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Observable;
 import java.util.Observer;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -27,6 +24,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
+import player.Player;
 /**
  *
  * @author Andrew Lee 17983766
@@ -36,73 +34,69 @@ public class HighscorePanel extends JPanel implements Observer{
     private final JButton back;
     private final GameState gameState;
     private final JTable table;
+    private ArrayList<Player> playerList;
+    private final JLabel highscoreTitleLabel;
+    private final JPanel highscoreTitlePanel;
     
     public HighscorePanel(int width, int height, GameState gameState, Color backgroundColor){
         this.gameState = gameState;
-
         
+        //set up title label
+        highscoreTitleLabel = new JLabel("HIGHSCORES");
+        highscoreTitleLabel.setFont(new Font("", Font.BOLD, 16));
+        highscoreTitleLabel.setSize(400,100);
+        highscoreTitleLabel.setForeground(Color.RED);
+        highscoreTitlePanel = new JPanel();
+        highscoreTitlePanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        highscoreTitlePanel.setBackground(Color.black);
+        highscoreTitlePanel.add(highscoreTitleLabel);
+        
+        //set up back button
         back = new JButton("Back");
         back.setMaximumSize(new Dimension(200, 75));
         back.setBackground(new Color(213, 53, 42));
         back.setForeground(Color.white);
         back.setFocusable(false);
         
-        // @Rhys, If you see this part, data variable is a 2d array
-        // with first column player names and second volumn highscores
-        // I need you to get these values from the database. ty
+        
+        
+//        String[][] data = {
+//            {"Andrew","5"}, {"Kerry","2"}, {"Cheema","3"},
+//            {"Carrie","14"}, {"Rhys","9"}, {"Steffan","1"}
+//        };
+
+        //Set up database data for JTable
         String[] columnNames = {"Player", "HighScore"};
-        String[][] data = {
-            {"Andrew","5"}, {"Kerry","2"}, {"Cheema","3"},
-            {"Carrie","14"}, {"Rhys","9"}, {"Steffan","1"}
-        };
-        
-        // Sort the array by the highest score
-        Arrays.sort(data, (a,b) ->{
-            String t1 = a[1];
-            String t2 = b[1];
-            int compare = Integer.compare(Integer.parseInt(t1), Integer.parseInt(t2));
-            if (compare == 0){
-                return 0;
-            }else if (compare > 0){
-                return -1;
-            }else{
-                return 1;
-            }
-        });
-        
+        String[][] data;
+        playerList = GameDBManager.getPlayerList();
+        data = toDataArray(playerList);
+       
+        //Set up the table panel and the JTable to represent data
         JPanel tablePanel = new JPanel();
+        tablePanel.setSize(400, 1000);
+        
+        //Set up table
         DefaultTableModel model = new DefaultTableModel(data, columnNames);
         table = new JTable(model);
-        //table.setSize(new Dimension(500, 250));
         table.setForeground(Color.red);
         //table.setPreferredScrollableViewportSize(new Dimension(450,63));
         table.setFillsViewportHeight(true);
         table.getColumnModel().getColumn(0).setPreferredWidth(100);
         table.getColumnModel().getColumn(1).setPreferredWidth(100);
+        
+        //Set up the table scroll bar
         JScrollPane scrollPane = new JScrollPane(table);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
         scrollPane.setVisible(true);
         tablePanel.add(scrollPane);
-        tablePanel.setSize(400, 1000);
         
-        JPanel jp = new JPanel();
-        jp.setBackground(Color.BLACK);
-        jp.setSize(200, 500);
-        
-        JTextField usernameEntry = new JTextField();
-        usernameEntry.setMaximumSize(new Dimension(300, 300));
-        usernameEntry.setBackground(backgroundColor);
-        usernameEntry.setForeground(Color.red);
-        usernameEntry.setCaretColor(Color.white);
-        usernameEntry.setFont(new Font("", Font.BOLD, 16));
-        
-        jp.add(usernameEntry);
+  
         
         
         super.setSize(width, height);
         super.setLayout(new BorderLayout());
         super.setBackground(backgroundColor);
         super.add(back, BorderLayout.SOUTH);
-        super.add(jp, BorderLayout.WEST);
         super.add(tablePanel, BorderLayout.CENTER);
     }    
     
@@ -118,11 +112,48 @@ public class HighscorePanel extends JPanel implements Observer{
         }
     }
     
+    /**
+     * takes a string and returns the related button
+     * @param btnName
+     * @return
+     * @throws IllegalArgumentException 
+     */
     public JButton getButton(String btnName) throws IllegalArgumentException{
         if(btnName.equalsIgnoreCase("back")){
             return this.back;
         }
         throw new IllegalArgumentException("Invalid button name");
+    }
+    
+    /**
+     * Transfer an arrayList of players into a 2d array with the player
+     * name and highscore. Sort the array by the greatest scores 
+     * @param playerList
+     * @return 2d array of playername and highscore
+     */
+    private String[][] toDataArray(ArrayList<Player> playerList){
+        // Transfer the arraylist of players into a 2d array named 
+        // data. Fill with player names and corresponding highscore
+        String[][] data = new String[playerList.size()][2];
+        for (int i = 0; i < playerList.size(); i++){
+            data[i][0] = playerList.get(i).getUsername();
+            data[i][1] = Integer.toString(playerList.get(i).getCurrentHighscore());
+        }
+        //Sort the array by the highest score
+        Arrays.sort(data, (a,b) ->{
+            String t1 = a[1];
+            String t2 = b[1];
+            int compare = Integer.compare(Integer.parseInt(t1), Integer.parseInt(t2));
+            if (compare == 0){
+                return 0;
+            }else if (compare > 0){
+                return -1;
+            }else{
+                return 1;
+            }
+        });
+        
+        return data;
     }
 }
 
